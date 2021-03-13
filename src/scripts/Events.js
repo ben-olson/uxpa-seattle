286b3d4689ef8d81904cb8cf44f726cf3c8a554b
Benjamin Olson <benjaminolson@Benjamins-MacBook-Pro.local>
2021-03-12 22:57:52 -0800

    Added No Event Placeholders

--- src/scripts/Events.js
+++ src/scripts/Events.js
@@ -26,8 +26,8 @@ function Events() {
     return undefined;
   }).filter(value => value !== undefined);
 
-  // console.log(eventsNew);
-  // console.log(eventsOld);
+  console.log(eventsNew);
+  console.log(eventsOld);
 
   return (
 
@@ -35,49 +35,54 @@ function Events() {
       <div className="[ events__new ] [ container ] [ flex:column ] [ flow ]">
         <h2 className="[ heading ]">Upcoming Events</h2>
         
-        {eventsNew.map((e,i) => {
+        {!eventsNew.length ? <p className="[ font-size:s-1 ]">No upcoming events.</p> :
+          eventsNew.map((e,i) => {
 
-          return (
+            return (
 
-            <a key={i} className="[ event ]" {... e.link ? {href: e.link} : {}} rel="no-opener noreferrer" target="_blank">
+              <a key={i} className="[ event ]" {... e.link ? {href: e.link} : {}} rel="no-opener noreferrer" target="_blank">
 
-              <section className={`[ card ${e.link ? 'card__hover' : ''}  ] [ flex ]`}>
-                <div style={{ backgroundImage:`url(${window.location.origin + e.thumbnail})` }}className="[ event__img ]"></div>
-                <div className="[ event__meta ] [ flow:tight ]">
-                  <h3 className="[ font-size:s1 ]">{e.title}</h3>
-                  <p className="[ caps ]"><span className="[ date ]">{e.date.toDateString()}</span>&nbsp;&middot;&nbsp;<span className="[ location ]">{e.location}</span></p>
-                  <p className="[ description ] [ font-size:s-1 ]">{e.excerpt}</p>
-                  <p className="[ source ][ font:bold font-size:s-1 ]">Created by {e.organization}</p>
-                </div>
-              </section>
+                <section className={`[ card ${e.link ? 'card__hover' : ''}  ] [ flex ]`}>
+                  <div style={{ backgroundImage:`url(${window.location.origin + e.thumbnail})` }}className="[ event__img ]"></div>
+                  <div className="[ event__meta ] [ flow:tight ]">
+                    <h3 className="[ font-size:s1 ]">{e.title}</h3>
+                    <p className="[ caps ]"><span className="[ date ]">{e.date.toDateString()}</span>&nbsp;&middot;&nbsp;<span className="[ location ]">{e.location}</span></p>
+                    <p className="[ description ] [ font-size:s-1 ]">{e.excerpt}</p>
+                    <p className="[ source ][ font:bold font-size:s-1 ]">Created by {e.organization}</p>
+                  </div>
+                </section>
 
-            </a>
+              </a>
 
-          )
-        })}
+            )
+          }
+        )}
 
       </div>
       <div className="[ events__old ][ container ] [ flex:column ]">
         <h2 className="[ heading ]">Past Events</h2>
-        {eventsOld.map((e,i) => {
 
-        return (
+        {!eventsOld.length ? <p className="[ font-size:s-1 ]">No past events.</p> :
+          eventsOld.map((e,i) => {
 
-          <a key={i} className="[ event ]" {... e.link ? {href: e.link} : {}} rel="no-opener noreferrer" target="_blank">
+            return (
 
-            <section className={`[ card ${e.link ? 'card__hover' : ''}  ] [ flex ]`}>
-              <div style={{ backgroundImage:`url(${window.location.origin + e.thumbnail})` }}className="[ event__img ]"></div>
-              <div className="[ event__meta ] [ flow:tight ]">
-                <h3 className="[ font-size:s1 ]">{e.title}</h3>
-                <p className="[ caps ]"><span className="[ date ]">{e.date.toDateString()}</span>&nbsp;&middot;&nbsp;<span className="[ location ]">{e.location}</span></p>
-                <p className="[ source ][ font:bold font-size:s-1 ]">Created by {e.organization}</p>
-              </div>
-            </section>
+              <a key={i} className="[ event ]" {... e.link ? {href: e.link} : {}} rel="no-opener noreferrer" target="_blank">
 
-          </a>
+                <section className={`[ card ${e.link ? 'card__hover' : ''}  ] [ flex ]`}>
+                  <div style={{ backgroundImage:`url(${window.location.origin + e.thumbnail})` }}className="[ event__img ]"></div>
+                  <div className="[ event__meta ] [ flow:tight ]">
+                    <h3 className="[ font-size:s1 ]">{e.title}</h3>
+                    <p className="[ caps ]"><span className="[ date ]">{e.date.toDateString()}</span>&nbsp;&middot;&nbsp;<span className="[ location ]">{e.location}</span></p>
+                    <p className="[ source ][ font:bold font-size:s-1 ]">Created by {e.organization}</p>
+                  </div>
+                </section>
 
-        )
-        })}
+              </a>
+
+            )
+          }
+        )}
 
       </div>
     </article>
